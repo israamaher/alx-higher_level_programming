10b22e2da63e43e8af7a5abdb1679ba393fd0782
Israa maher <you@example.com>
2023-11-02 08:50:03 -0700

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 from add_0 import add
 a = 1
 b = 2
-print(f"{a:d} + {b:d} = {add(a, b):d}")
+result = add(a, b)
+print(f"{a:d} + {b:d} = {result:d}")
