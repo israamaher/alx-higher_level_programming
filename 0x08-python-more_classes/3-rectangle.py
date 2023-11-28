cd0b8132790277cfd11f479d4399639a1577ce4d
israa maher <you@example.com>
2023-11-27 18:49:40 -0800

    str

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """makes rectangle object"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -11,16 +12,19 @@ class Rectangle:
     @property
     def width(self):
         """gets width"""
+
         return self.__width
 
     @property
     def height(self):
         """gets height"""
+
         return self.__height
 
     @width.setter
     def width(self, value):
         """sets width"""
+
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
@@ -30,6 +34,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """sets height"""
+
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
