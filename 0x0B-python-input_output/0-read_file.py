3579555c442521c773bc7f04e2a1efd40316a1d8
israa maher <you@example.com>
2023-12-05 15:53:14 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,6 @@ reads a text file
 
 
 def read_file(filename=""):
-    with open(filename, encoding="UTF8") as f:
-        print(f.read(), end="")
+    with open(filename, mode='r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
