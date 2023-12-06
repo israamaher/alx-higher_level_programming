ed9f070784732be33da71d46fb65f93be0722c71
israa maher <you@example.com>
2023-12-05 16:19:59 -0800

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,6 @@
 
 
 def append_write(filename="", text=""):
+    """append file """
     with open(filename, mode="a+", encoding="UTF8") as f:
-        return f.write(text)
+        return(f.write(text))
