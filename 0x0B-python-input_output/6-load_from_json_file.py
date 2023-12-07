a1618852216dc6e162eb5ed9fb642cb0540b6d2b
israa maher <you@example.com>
2023-12-06 16:09:57 -0800

    2 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,10 @@ import json
 
 
 def load_from_json_file(filename):
-    """does it"""
+    """
+    loads an object from json file
+    -filename: name of file
+    -Return: an object
+    """
     with open(filename, mode='r', encoding='utf-8') as f:
         return(json.load(f))
