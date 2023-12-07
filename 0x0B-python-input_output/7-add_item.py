1b95b07d73c2e6fa2b5cedd656ddf096c103f150
israa maher <you@example.com>
2023-12-06 16:26:34 -0800

    7 0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,11 +5,11 @@ import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+listarg= list(sys.argv[1:])
 try:
     jsonList = load_from_json_file("add_item.json")
 except:
     jsonList = []
 
-for i in sys.argv[1:]:
-    jsonList.append(i)
+jsonlist.extend(listarg)
 save_to_json_file(jsonList, "add_item.json")
