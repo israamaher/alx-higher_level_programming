e3e2e90fe98d18eb6871a1452f5a62565af60895
israa maher <you@example.com>
2023-12-13 15:34:34 -0800

    new rec

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,89 +1,102 @@
 #!/usr/bin/python3
-"""Class Rectangle"""
+""" rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """class Rectangle that inherits from Base"""
+    """makes a new rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-         """initializes"""
-        super().__init__(id)
+        """initializes"""
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
+
+    def to_dictionary(self):
+        """dictionary"""
+        dic = {'id': self.id, 'width': self.width, 'height': self.height,
+               'x': self.x, 'y': self.y}
+        return dic
+
+    def update(self, *args, **kwargs):
+        """updates attributes"""
+        if args:
+            i = 0
+            keys = ['id', 'width', 'height', 'x', 'y']
+            for arg in args:
+                setattr(self, keys[i], arg)
+                i += 1
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+    def validator(self, name, value):
+        """validates the shit out of it"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if (name is 'width' or name is 'height') and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        if (name is 'x' or name is 'y') and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
 
     @property
     def width(self):
+        """get width"""
         return self.__width
-
-    @width.setter
-    def width(self, value):
-        self.validator("width", value)
-        self.__width = value
 
     @property
     def height(self):
+        """get height"""
         return self.__height
-
-    @height.setter
-    def height(self, value):
-        self.validator("height", value)
-        self.__height = value
 
     @property
     def x(self):
+        """get x"""
         return self.__x
+
+    @property
+    def y(self):
+        """get y"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """sets width"""
+        self.validator("width", value)
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """sets height"""
+        self.validator("height", value)
+        self.__height = value
 
     @x.setter
     def x(self, value):
         self.validator("x", value)
+        """sets x"""
         self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
 
     @y.setter
     def y(self, value):
+        """sets y"""
         self.validator("y", value)
         self.__y = value
 
-    def validator(self, name, value):
-        """validateor the input """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if (name == 'width' or name == 'height') and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        if (name == 'x' or name == 'y') and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
-
     def area(self):
-        """return area of rectangle"""
-        return (int(self.width) * int(self.height))
+        """gets area"""
+        return self.width * self.height
 
     def display(self):
-        """ print the rectangle """
-        print('\n'*self.y, end="")
-        for i in range(self.height):
-            row = "".join(" " * self.x + "#" * self.width)
-            print(row)
+        """prints a rectangle of hashes"""
+        print('\n' * self.y, end="")
+        print(''.join(' ' * self.x + '#' * self.width + '\n'
+                      for times in range(self.height)), end="")
 
     def __str__(self):
-        """ return rectangle """
+        """gets rectangle"""
         return "[{}] ({}) {}/{} - {}/{}".format(
-                type(self).__name__, self.id, self.x, self.y,
-                self.width, self.height)
-
-    def update(self, id=None, width=None, height=None, x=None, y=None):
-        """ update the argument """
-        if id is not None:
-            self.id = id
-        if width is not None:
-            self.width = width
-        if height is not None:
-            self.height = height
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
+            type(self).__name__, self.id, self.x, self.y,
+            self.width, self.height)
